944c876a440d41baa73e271634e3f46e66c740f1
Anghel Melanie Trillo Hernández <59265128+AM27TH@users.noreply.github.com>
2021-06-15 09:06:28 -0500

    Tests de pruebas unitarias

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -39,9 +39,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
-    // });
+    it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+      expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+    });
 
     // Hacker edition
     //
@@ -53,9 +53,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // it('should return " !@" for " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
+    it('should return " !@" for " !@"', () => {
+      expect(cipher.encode(33, ' !@')).toBe(' !@');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -86,9 +86,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
+    it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+      expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+    });
 
     // Hacker edition
     //
@@ -100,9 +100,34 @@ describe('cipher', () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // it('should return " !@" para " !@"', () => {
-    //   expect(cipher.decode(33, ' !@')).toBe(' !@');
-    // });
-  });
+    it('should return " !@" para " !@"', () => {
+      expect(cipher.decode(33, ' !@')).toBe(' !@');
+    });
 
+    // Hacker edition
+    //
+    // [Español]
+    // Si decides implementar soporte offset negativo descomenta
+    // el test a continuación.
+    //
+    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset -27', () => {
+      expect(cipher.encode(-27, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('ZABCDEFGHIJKLMNOPQRSTUVWXY');
+    });
+    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset -27', () => {
+      expect(cipher.decode(-27, 'ZABCDEFGHIJKLMNOPQRSTUVWXY')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    // Hacker edition
+    //
+    // [Español]
+    // Si decides implementar soporte offset negativo descomenta
+    // el test a continuación.
+    //
+    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset -27', () => {
+      expect(cipher.encode(1, '123')).toBe('234');
+    });
+    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset -27', () => {
+      expect(cipher.decode(1, '123')).toBe('012');
+    });
+  });
 });
